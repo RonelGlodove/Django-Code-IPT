664536060140ff4dd0d02b413338a274f699e5d2
Glodove Ronel B. <yukiborres9@gmail.com>
2026-04-06 10:54:41 +0800

    Delete skechers_advanced directory

--- skechers_advanced/manage.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import os,sys
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','skechers.settings')
-from django.core.management import execute_from_command_line
-execute_from_command_line(sys.argv)
